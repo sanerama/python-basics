3028e45ee17e4b1e4f8dc8a2697e438ace4531a7
sanerama <ramasane99@gmail.com>
2018-10-14 22:28:27 +0100

    loop tutorial

---
+++ loopTutorial.py
@@ -0,0 +1,13 @@
+# A Sample Python program to show loop (unlike many 
+# other languages, it doesn't use ++) 
+for i in range(0, 5): 
+    print(i) 
+
+for i in range(0, 5): 
+    print(i) 
+i = 0
+while(i<5):
+    print(i)
+    print("printing second line inside loop")
+    i+=1
+print("End of program")
